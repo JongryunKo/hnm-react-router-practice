9b0e73c7c8f17bbade64f37ec454fe92f4386433
JongryunKo <secwind1206@gmail.com>
2024-08-22 01:30:39 +0900

    first commit

---
+++ src/component/ProductCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <img src='https://lp2.hm.com/hmgoepprod?set=source[/e0/ee/e0ee7117a23f0accd4d56f09be2bc8e392df2228.jpg],origin[dam],category[men_shirts_longsleeved],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]' />
+        <div>Conscious choice</div>
+        <div>벨티드 트월 코트</div>
+        <div>$90</div>
+    </div>
+  )
+}
+
+export default ProductCard
